cd0bda7b29a6e165cb50e8f2f2f9d4ba2c702667
Kaniz Fatema <138873297+kaniz-codes@users.noreply.github.com>
2024-09-30 10:32:22 +0600

    Create 03_Teacher has three attributes (Name, short code, Dept).cpp

---
+++ Practice /03_Teacher has three attributes (Name, short code, Dept).cpp
@@ -0,0 +1,27 @@
+/* 2. A class named Teacher has three attributes (Name, short code, Dept).
+Initialize the attributes using a member function called set_value() and display them using get_value().
+Create one object of the Teacher class. */
+
+#include <iostream>
+using namespace std;
+class Teacher{
+    public:
+    string Name, shortcode, Dept;
+
+    void set_value()
+    {
+        cin>>Name>>shortcode>>Dept;
+    }
+    
+    void get_value()
+    {
+        cout<<Name<<endl<<shortcode<<endl<<Dept<<endl;
+    }
+};
+int main() 
+{
+    Teacher obj;
+    obj.set_value();
+    obj.get_value();
+    return 0;
+}
