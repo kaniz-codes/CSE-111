625bcc633c3626a838010c1cfb8173d2cf38a052
Kaniz Fatema <138873297+kaniz-codes@users.noreply.github.com>
2024-09-30 13:02:02 +0600

    Create 03_Sum-of-two-numbers-using-user-input.cpp

---
+++ Practice /03_Sum-of-two-numbers-using-user-input.cpp
@@ -0,0 +1,12 @@
+/*Sum of two numbers using user input */
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a,b,sum=0;
+    cin>>a>>b;
+    sum=a+b;
+    cout<<sum<<endl;
+    return 0;
+}
