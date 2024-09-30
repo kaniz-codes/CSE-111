c4da546dc80a2eb4a1d2ced330824449ad4c6322
Kaniz Fatema <138873297+kaniz-codes@users.noreply.github.com>
2024-09-30 17:54:15 +0600

    Update 2a)_Design a Product class.cpp

--- Mid CT Qus Solve/2a)_Design a Product class.cpp
+++ Mid CT Qus Solve/2a)_Design a Product class.cpp
@@ -2,37 +2,37 @@
 Implement a function called setProduct() within the class to input the product details from the user. Additionally, create a function named printProduct() to display the product details. Ensure that setProduct() is not directly called in the main() function.
 Add a destructor to print "Product object destroyed" when an object is destroyed. */
 
-#include <iostream>
+#include<iostream>
 using namespace std;
-class Product 
-{
-    string name;
-    float price;
-    int quantity;
+
+class product{
 public:
-    void setProduct() 
-    {
-        cout << "Enter product name: ";
-        cin >> name;
-        cout << "Enter product price: ";
-        cin >> price;
-        cout << "Enter product quantity: ";
-        cin >> quantity;
+    string name;
+    int price,quantity;
+
+    void setProduct(){
+        cin>>name>>price>>quantity;
     }
-    void printProduct() 
-    {
-        cout << "Product Name: " << name << endl;
-        cout << "Product Price: " << price << endl;
-        cout << "Product Quantity: " << quantity << endl;
+
+    void printProduct(){
+        cout<<name<<endl<<price<<endl<<quantity<<endl;
     }
-    ~Product() {
-        cout << "Product object destroyed." << endl;
+
+    void callProduct(){
+        setProduct();
+    }
+
+    ~product(){
+        cout<<"product object destroyed"<<endl;
     }
 };
-int main() 
-{
-    Product p;
-    p.setProduct();
-    p.printProduct();
+
+int main(){
+    product p1;
+
+    p1.callProduct();
+    p1.printProduct();
+
+
     return 0;
 }
