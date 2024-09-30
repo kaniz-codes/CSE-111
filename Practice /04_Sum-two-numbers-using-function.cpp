a8f7c1f1367e8e8f38d45f87d758544a4bf6750a
Kaniz Fatema <138873297+kaniz-codes@users.noreply.github.com>
2024-09-30 13:06:34 +0600

    Create 04_Sum-two-numbers-using-function.cpp

---
+++ Practice /04_Sum-two-numbers-using-function.cpp
@@ -0,0 +1,17 @@
+/*Sum two numbers using function */
+
+#include <iostream>
+using namespace std;
+void sum()
+{
+    int a,b,sum=0;
+    cin>>a>>b;
+    //sum=a+b;
+    //cout<<sum<<endl;
+    cout<<a+b<<endl;
+}
+int main() 
+{
+    sum();
+    return 0;
+}
