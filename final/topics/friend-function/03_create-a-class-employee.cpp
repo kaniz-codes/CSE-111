41589304eae80fdf97c51a8431be9824e6579ec9
Kaniz Fatema <138873297+kaniz-codes@users.noreply.github.com>
2024-10-29 16:32:19 +0600

    Create 03_create-a-class-employee.cpp

---
+++ final/topics/friend-function/03_create-a-class-employee.cpp
@@ -0,0 +1,36 @@
+/* Create a class Employee with private members Employee_id and salary.
+Write a friend function named displayEmployeeInfo (outside the class) that can access these private members and print out the employee's information.*/
+
+#include<iostream>
+using namespace std;
+
+class Employee
+{
+    int Employee_id, salary;
+
+public:
+    Employee(int i, int s)
+    {
+        Employee_id=  i;
+        salary= s;
+    }
+
+    friend void displayEmployeeInfo(Employee obj);
+};
+
+void displayEmployeeInfo(Employee obj)
+{
+    cout<<"Employee_id: "<<obj.Employee_id<<endl<<"salary: "<<obj.salary<<endl;
+}
+
+int main()
+{
+    int i,s;
+    cout<<"Employee ID: ";
+    cin>>i;
+    cout<<"salary: ";
+    cin>>s;
+    Employee obj(i,s);
+    displayEmployeeInfo(obj);
+}
+
