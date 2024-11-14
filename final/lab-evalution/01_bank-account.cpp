821ba436a74b057cc994e8ebfe27a42eb2c26e8f
Kaniz Fatema <138873297+kaniz-codes@users.noreply.github.com>
2024-11-14 21:57:44 +0600

    Update 01_bank-account.cpp

--- final/lab-evalution/01_bank-account.cpp
+++ final/lab-evalution/01_bank-account.cpp
@@ -1,1 +1,56 @@
+/* Implement a BankAccount class where each account has a balance. Overload + and - operators to deposit and withdraw money.*/
+
+#include<iostream>
+using namespace std;
+class BankAccount
+{
+public:
+    int balance;
+
+    BankAccount()
+    {
+
+    }
+    BankAccount(int x)
+    {
+        balance=x;
+
+    }
+    BankAccount operator+(int x)
+    {
+        BankAccount op;
+        op.balance= balance+x;
+        return op;
+    }
+
+    BankAccount operator - (int x)
+    {
+        BankAccount op;
+        op.balance= balance-x;
+        return op;
+    }
+};
+
+int main()
+{
+    BankAccount obj(430),obj1(52),obj3(0);
+    int a;
+    cout<<"enter 1 for diposit or 2 for withdraw: ";
+    cin>>a;
+
+    if(a==1)
+    {
+        cout<<"Enter number to  diposit: ";
+        cin>>a;
+        obj3= obj+a;
+        cout<<"Diposit: "<<obj3.balance<<endl;
+    }
+    else if(a==2)
+    {
+        cout<<"Enter number to withdraw: ";
+        cin>>a;
+        obj3= obj-a;
+        cout<<"withdraw: "<<obj3.balance<<endl;
+    }
+}
 
