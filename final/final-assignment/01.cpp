a0dd58adc5172c89b57fb6fbbca0f91715c5f224
Kaniz Fatema <138873297+kaniz-codes@users.noreply.github.com>
2024-12-01 22:23:07 +0600

    Update 01.cpp

--- final/final-assignment/01.cpp
+++ final/final-assignment/01.cpp
@@ -1,1 +1,30 @@
+// Difference between pass value and pass by reference with coding example.
 
+#include <iostream>
+using namespace std;
+
+class square
+{
+    public:
+    int width;
+    square()
+    {
+        
+    }
+    square(int x)
+    {
+        width=x;
+    }
+    square(square &s)
+    {
+        width = s.width-5;
+    }
+};
+int main()
+{
+    square s1, s2(8), s3(s2);
+    cout<<"Pass value: "<<s2.width<<endl;
+    cout<<"Pass : "<<s3.width<<endl;
+    
+    return 0;
+}
