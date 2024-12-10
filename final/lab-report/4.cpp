3a4a19635138c3c89f09f760ac372b460a44fcf5
Kaniz Fatema <138873297+kaniz-codes@users.noreply.github.com>
2024-12-10 21:50:16 +0600

    Update and rename 3.cpp to 4.cpp

--- final/lab-report/4.cpp
+++ final/lab-report/4.cpp
@@ -1,41 +1,33 @@
 #include <iostream>
-#include <string>
 using namespace std;
-
 class Library {
 protected:
     string lib_name, location;
-
 public:
     void set_library(string n, string l) {
         lib_name = n;
         location = l;
     }
 };
-
 class Section : public Library {
 protected:
     string section_name;
     int section_code;
-
 public:
     void set_section(string sec_name, int sec_code) {
         section_name = sec_name;
         section_code = sec_code;
     }
 };
-
 class Book : public Section {
 private:
     string book_name, author, isbn;
-
 public:
     void set_book_details(string b_name, string b_author, string b_isbn) {
         book_name = b_name;
         author = b_author;
         isbn = b_isbn;
     }
-
     void display_info() {
         cout << "Library Name: " << lib_name << endl;
         cout << "Location: " << location << endl;
@@ -46,7 +38,6 @@ public:
         cout << "ISBN: " << isbn << endl;
     }
 };
-
 int main() {
     Book obj;
     obj.set_library("Kaniz er Library", "Mirpur");
@@ -55,6 +46,5 @@ int main() {
 
     obj.display_info();
     cout << endl << "Name: Kaniz Fatema" << endl << "ID: 20245103154" << endl;
-
     return 0;
 }
